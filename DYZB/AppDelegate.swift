33c2c3b6debd325bb063214a32f81741cf618358
zhufeng <358244144@qq.com>
2020-02-10 08:49:31 +0800

    第一次提交代码

---
+++ DYZB/AppDelegate.swift
@@ -0,0 +1,13 @@
+//  AppDelegate.swift
+//  DYZB
+//  Created by xiaohuang on 2020/2/10.
+//  Copyright © 2020 xiaohuang. All rights reserved.
+import UIKit
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
